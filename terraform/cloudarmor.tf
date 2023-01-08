b15f2f09e509e3c55030f695320662d452edbe7c
vikcloud <vik.shinde@gmail.com>
2023-01-08 15:15:21 +0000

    RDS-1 added terraform

---
+++ terraform/cloudarmor.tf
@@ -0,0 +1,30 @@
+resource "google_compute_security_policy" "policy" {
+  name = "allow-my-ip"
+
+  rule {
+    action   = "allow"
+    priority = "0"
+
+    match {
+      versioned_expr = "SRC_IPS_V1"
+
+      config {
+        src_ip_ranges = ["${var.allow_ips}"]
+      }
+    }
+  }
+
+  rule {
+    action   = "deny(403)"
+    priority = "2147483647"
+    description = "Default rule: deny all"
+
+    match {
+      versioned_expr = "SRC_IPS_V1"
+
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+  }
+}
