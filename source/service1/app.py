88412576b80b8a60a519ffe1eff5d9177b7fc632
Vikram Shinde <Vikram.Shinde@gfs.com>
2023-01-19 17:15:52 +0000

    RDS-12 Adding pytest and code coverage

--- source/service1/app.py
+++ source/service1/app.py
@@ -10,4 +10,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
